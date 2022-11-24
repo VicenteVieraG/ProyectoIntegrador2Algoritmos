541995fc7598a5a0b50a61d3fe0850eae0d02a4b
Vicente Viera <plantedcoot706@gmail.com>
2022-11-24 13:40:34 -0600

    Animations added to the intro

--- Poster/src/components/Header.tsx
+++ Poster/src/components/Header.tsx
@@ -4,7 +4,14 @@ import "../styles/main.css";
 const Header = () => {
   return (
 	<div className="headerContainer">
-		<p>Poster Argumentativo</p>
+		<p className="title">Poster Argumentativo</p>
+		<div className="intro">
+			<ul className="noDots">
+				<li><p className="names">Vicente Viera Guízar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;A01639784</p></li>
+				<li><p className="names">Renet de Jesús Pérez Gómez&nbsp;|&nbsp;A01640555</p></li>
+				<li><p className="names">Carlos García Mercado&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;A01633757</p></li>
+			</ul>
+		</div>
 	</div>
   )
 }
