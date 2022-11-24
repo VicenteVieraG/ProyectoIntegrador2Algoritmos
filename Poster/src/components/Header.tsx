6bc5cb64d6aba8eb2964a174f628271acf143b99
Vicente Viera <plantedcoot706@gmail.com>
2022-11-24 08:03:33 -0600

    Basic file Sistem Created for Poster

---
+++ Poster/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/main.css";
+
+const Header = () => {
+  return (
+	<div className="headerContainer">
+		<p>Poster Argumentativo</p>
+	</div>
+  )
+}
+
+export default Header
